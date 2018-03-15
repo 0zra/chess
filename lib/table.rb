7a8e575f4bb8e38c4a042d56104dc51d4a48d399
ozra <petar.ozretic@mail.com>
2018-03-15 16:00:57 +0100

    Integrate basic move method

--- lib/table.rb
+++ lib/table.rb
@@ -6,9 +6,10 @@ class Table
     (:A..:H).each do |letter|
       @state[letter] = Array.new(8, nil)
     end
-
-    @state[:B] = Array.new(8,"\u265F")
+    @state[:H] = ["\u2655","\u2658","\u2657","\u2654","\u2655","\u2657","\u2658","\u2655"]
     @state[:G] = Array.new(8,"\u2659")
+    @state[:B] = Array.new(8,"\u265F")
+    @state[:A] = ["\u265C","\u265E","\u265D","\u265B","\u265A","\u265D","\u265E","\u265C"]
   end
 
   def print_table
@@ -25,7 +26,32 @@ class Table
     puts "\t\t     1   2   3   4   5   6   7   8  "
   end
 
+  def move from, to
+    rf = from[0]
+    cf = from[1].to_i
+    @state[rf.to_sym][cf-1] = nil
+  end
 end
+
+
 
 t = Table.new
 t.print_table
+print "Koga micemo:"
+who = gets.chomp
+t.move who, "nesto"
+t.print_table
+=begin
+t.state[:B][0] = nil
+gets
+t.state[:D][0] = "\u265F"
+t.print_table
+gets
+t.state[:G][1] = nil
+t.state[:E][1] = "\u2659"
+t.print_table
+gets
+t.state[:D][0] = nil
+t.state[:E][1] = "\u265F"
+t.print_table
+=end
